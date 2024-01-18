b5a918a506aacc103520959217a8ac6170856105
Nayel Blidi <nayel.blidi@ipsa.fr>
2024-01-18 19:17:34 +0100

    N/A

--- scripts/start_game_windows.bat
+++ scripts/start_game_windows.bat
@@ -13,14 +13,14 @@ rem Directory of the game
 set "jeu_dossier=C:\Users\nayel\Documents\Git\Robot\scripts"
 
 rem Launch the server in the first command prompt window
-start "Serveur" cmd /K "cd /D %jeu_dossier% && python server.py -nb %nb_joueurs%"
+start "Serveur" cmd /K "cd /D %jeu_dossier% && python server.py -nb %nb_joueurs%" || exit /B 1
 
 rem Wait for a moment to allow the server to start
 timeout /T 2 /NOBREAK >nul
 
 rem Launch the agents in subsequent command prompt windows
 for /L %%i in (1, 1, %nb_joueurs%) do (
-    start "Agent %%i" cmd /K "cd /D %jeu_dossier% && python agent.py"
+    start "Agent %%i" cmd /K "cd /D %jeu_dossier% && python agent.py" || exit /B 1
 )
 
 exit /B 0
